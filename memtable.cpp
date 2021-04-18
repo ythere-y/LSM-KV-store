c69c5bd8603a1e1b8c0c30a55f72696242e23d05
hc_PC <1069043992@qq.com>
2021-04-18 21:18:05 +0800

    完成memtable

--- memtable.cpp
+++ memtable.cpp
@@ -7,17 +7,16 @@ MemTable::MemTable()
 
 int MemTable::insert(uint64_t key, const std::string &s){
 
+    sl->insertNode(key,s);
     return 1;
 }
 
 std::string MemTable::search(uint64_t key){
-
-        return "";
-
+    SKNode_mem* get = sl->searchNode(key);
+    return *get->offset;
 }
 
 bool MemTable::remove(uint64_t key){
-    return false;   //删除失败
-
+    return sl->deleteNode(key);
 }
 
